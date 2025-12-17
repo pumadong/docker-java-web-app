035728fc1264b68b0e1506d86f6aac46f3cdf603
puma.dong@hotmail.com <puma.dong@hotmail.com>
2025-12-17 20:44:09 +0800

    first commit

---
+++ src/main/java/com/bob/dockerjavawebapp/DockerJavaWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.bob.dockerjavawebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerJavaWebAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DockerJavaWebAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bob/dockerjavawebapp/DockerJavaWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bob.dockerjavawebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerJavaWebAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
